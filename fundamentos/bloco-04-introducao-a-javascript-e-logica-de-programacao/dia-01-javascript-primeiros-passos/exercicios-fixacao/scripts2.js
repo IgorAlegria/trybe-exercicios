2bf151b754f578680a91d6c69fc9accc714c4302
Igor Alegria <igor.alegria@gmail.com>
2022-07-22 11:37:15 -0300

    Realizando o exercício de fixação 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-fixacao/scripts2.js
@@ -0,0 +1,13 @@
+// 1 Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const base = 5;
+let height = 8;
+
+// 2 Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const area = base * height;
+
+console.log(area);
+// Resultado 40.
+
+// 3 Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = base * 2 + height * 2;
+console.log(perimeter)
