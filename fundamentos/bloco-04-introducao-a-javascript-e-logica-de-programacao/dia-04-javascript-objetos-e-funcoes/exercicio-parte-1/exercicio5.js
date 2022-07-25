4b48cdd8a8b447bd972de452452eec319ed64ec2
Igor Alegria <igor.alegria@gmail.com>
2022-07-25 19:18:35 -0300

    Realizando exercícios 5 e 6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-parte-1/exercicio5.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: ['Margarida', 'Tio Patinhas'],
+    origem: ['Pato Donald', "Christmas on Bear Mountain, Dell's Four Color Comics #178"],
+    nota: ['Namorada do personagem principal nos quadrinhos do Pato Donald', 'O último MacPatinhas'],
+    recorrente: ['Sim', 'Sim'],
+  };
+  
+console.log(info.personagem[0] + ' e ' + info.personagem[1]);
+console.log(info.origem[0] + ' e ' + info.origem[1])
+console.log(info.nota[0] + ' e ' + info.nota[1])
+console.log('Ambos recorrentes')
